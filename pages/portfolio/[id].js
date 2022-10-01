8aecf0ea4220e2402d034b25ea32c7bb2256ce10
elluis <elluis.codes@gmail.com>
2022-10-01 17:45:55 -0500

    basic dynamic link setup - with no specific route in place using 'next/router' -

---
+++ pages/portfolio/[id].js
@@ -0,0 +1,14 @@
+// this is just a placeholder for dynamic routes - any extension of /portfolio/anything_here
+// will direct to this single page
+
+
+export default function Project_1 () {
+
+    return (
+
+        <div>
+            <h1>this is project 1 page</h1>
+        </div>)
+
+
+}
