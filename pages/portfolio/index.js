d7bf76bba1a6994a66902bf2ba7ecc3e82835134
elluis <elluis.codes@gmail.com>
2022-10-01 16:46:06 -0500

    next.js routing examples - completed nested folder routing example -

---
+++ pages/portfolio/index.js
@@ -0,0 +1,10 @@
+export default function Portfolio () {
+
+    return (
+        <div>
+            <h1>this is a portfolio</h1>
+        </div>
+    )
+
+
+}
---
+++ pages/portfolio/list.js
@@ -0,0 +1,11 @@
+function ListPage () {
+
+
+    return (
+        <div>
+            <h1>this list</h1>
+        </div>
+    )
+}
+
+export default ListPage;
