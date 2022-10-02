6bfe7fa85b0ee3a0159d1547450e47b728d0030d
elluis <elluis.codes@gmail.com>
2022-10-01 19:34:24 -0500

    set up a custome 404 page - file must be placed verbatim in ::  /pages/404.js

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+// custom 404 error file must be named 404.js for next.js to pick up
+
+export default function NotFoundPage() {
+
+
+
+    return(
+        <div>
+            <h1>this is a custom page not found</h1>
+        </div>
+    )
+}
