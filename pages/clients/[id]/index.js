25989a9fc737415ec5bc960ee97c044d9f450395
elluis <elluis.codes@gmail.com>
2022-10-01 19:29:28 -0500

    setting up programmatic links - e.g. using a button or an anchor to set up a link -

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,13 +1,33 @@
-
+import { useRouter } from "next/router";
 
 
 export default function ClientProject_1 () {
 
+    const router = useRouter();
+
+
+    function loadProjectHandler () {
+        console.log (router.query);
+
+        // router.push('/');
+        // router.replace() will not allow the user to go back
+
+
+        // yet another way to set up a link
+        router.push({
+            pathname: '/clients/[id]/[client-project-id]',
+            query: {id: 'max', clientprojectid: 'project'},
+        });
+
+
+    }
 
 
     return (
         <div>
             <h1>client project 1</h1>
+            {/* setting up a programmatic link */}
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     )
 }
