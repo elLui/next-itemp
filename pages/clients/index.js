64d8369cec2f453a1979e5f14eef2e5112f3e4b9
elluis <elluis.codes@gmail.com>
2022-10-01 19:13:50 -0500

    simple link clients/Link map example complete -

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,14 +1,28 @@
-
-
-
+import Link from "next/link";
 
 export default function Clients () {
 
+    const clients = [
+        { id: 'lui', name: 'elluis' },
+        { id: 'deb', name: 'debbie' },
+    ];
 
 
     return (
         <div>
             <h1>soy el client</h1>
+
+            <ul>
+                { clients.map ((client) => {
+                    return (
+                        <li key={ client.id }>
+                            <Link href={ `/clients/${ client.id }` }>{ client.name }</Link>
+                        </li>
+                    )
+                }) }
+            </ul>
+
+
         </div>
     );
 }
